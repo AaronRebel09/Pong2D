91141b2c952a6517c7c73bf145742a8867b92e32
Rodrigo Aaron <flamasrebel01@gmail.com>
2017-09-22 22:44:30 -0500

    Assets
    First version in unity 2d pong game

---
+++ CollisionScore.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScore : MonoBehaviour {	
+	public GameObject Enemigo;
+	public GameObject Player;
+
+	public AudioSource audiosource;
+	public AudioClip goalsound;
+
+	void OnCollisionEnter2D(Collision2D col){
+		Debug.LogError ("ColisionTriger "+col.collider.name);
+				if (col.collider.name == "Ball" ) {
+					Debug.LogError ("Choca Ball con Goal");
+					//sound Goal
+					audiosource.PlayOneShot(goalsound);
+					Score scriptEnemigo = Enemigo.GetComponent<Score> ();
+					scriptEnemigo.score++;
+					if (scriptEnemigo.score == 5) {
+						Debug.LogError (""+Enemigo+" You Win!");
+						//Restart Marcadores
+						Score scriptPlayer = Player.GetComponent<Score> ();
+						scriptEnemigo.score = 0;
+						scriptPlayer.score = 0;
+					}
+				}
+	}
+
+//	void OnTriggerEnter2D(Collider2D col){
+//		Debug.LogError ("ColisionTriger "+col.name);
+//		if (col.name == "Ball" ) {
+//			Debug.LogError ("Choca Ball con Goal");
+//			Score scriptEnemigo = Enemigo.GetComponent<Score> ();
+//			scriptEnemigo.score++;
+//			if (scriptEnemigo.score == 5) {
+//				Debug.LogError (""+Enemigo+" You Win!");
+//				//Restart Marcadores
+//				Score scriptPlayer = Player.GetComponent<Score> ();
+//				scriptEnemigo.score = 0;
+//				scriptPlayer.score = 0;
+//			}
+//		}
+//	}
+		
+}
---
+++ Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	public int score= 0;
+	public int x, y;
+
+	void OnGUI(){
+		GUI.Label (new Rect(x,y,100,200),"Score "+score);
+	}
+
+}
